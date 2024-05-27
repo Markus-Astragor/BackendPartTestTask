f6874c252dcbbe6c11553d8acf49b517bdfca043
Marko Kosyk <kosyk.markian@gmail.com>
2024-05-27 11:55:08 +0300

    changes for dockerization app and redis separated files

--- app/redisClient.js
+++ app/redisClient.js
@@ -2,6 +2,8 @@ const redis = require('redis');
 const redisClient = redis.createClient();
 
 redisClient.on('error', (err) => console.log('Redis client err', err));
-redisClient.connect();
+redisClient.connect({
+  url: 'redis://redis_database:6379'
+});
 
 module.exports = { redisClient }
